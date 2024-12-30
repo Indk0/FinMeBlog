919e299975ad213045f9ca16825981673ee55a4f
Inder Kumar <inderik302@gmail.com>
2024-12-30 13:43:15 +0000

    Refined post model and tested admin panelclear

--- fin_blog/models.py
+++ fin_blog/models.py
@@ -12,4 +12,11 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ["-created_on"]
 
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
