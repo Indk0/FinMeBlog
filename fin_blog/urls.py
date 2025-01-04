56f4e085eedad514d665bf10a70fc591a91f2ecd
Inder Kumar <inderik302@gmail.com>
2025-01-04 09:53:44 +0000

    Fix url path link for blogs

--- fin_blog/urls.py
+++ fin_blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='home'),
-    path('post/<slug:slug>/', views.post_detail, name='post_detail'),
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
     path('post/new/', views.create_post, name='create_post'),
     path('post/<slug:slug>/comment/', views.add_comment, name='add_comment'),
 ]
