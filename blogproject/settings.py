8d20d04c3b052416d3621590fde47e94c2dbb58c
Inder Kumar <inderik302@gmail.com>
2025-01-08 14:35:53 +0000

    Fix settings code issue

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -31,8 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    '8000-indk0-finmeblog-b79je5j4weg.ws.codeinstitute-ide.net', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-indk0-finmeblog-b79je5j4weg.ws.codeinstitute-ide.net', '.herokuapp.com']
 
 
 # Application definition
