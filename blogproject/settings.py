8fd546828daeaf22dc42d1481a41da1be9e6cd5c
Inder Kumar <inderik302@gmail.com>
2025-01-08 14:54:46 +0000

    Add code to fix error 500 issue

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -83,7 +83,7 @@ ROOT_URLCONF = 'blogproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
